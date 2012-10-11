af8ab16284981e02e302ee44e3760c61f9c4605a
Ramon Gonzalez <rjgonza@gmail.com>
2012-10-11 10:54:48 -0400

    more work

--- LearnPythonTheHardWay/ex45/game.py
+++ LearnPythonTheHardWay/ex45/game.py
@@ -6,9 +6,21 @@ print "Welcome! Please enter your name below"
 my_name = raw_input("Name: ")
 my_player = player(my_name)
 
-room1 = room("Shoreline")
-room2 = room("Forest")
-room3 = room("Clif")
+shore_scene= """
+shore
+"""
+
+forest_scene="""
+forest
+"""
+
+clif_scene="""
+clif
+"""
+
+room1 = room("Shoreline", shore_scene)
+room2 = room("Forest", forest_scene)
+room3 = room("Clif", clif_scene)
 
 print "%s, you were just on an amazing cruise, but the ship was poorly managed. " % my_player.whoami()
 print "As a result the crew was not taken care of and in turn did not take work hard"
@@ -16,7 +28,7 @@ print "or take care of the ship, the culmination of these efforts, or lack there
 print "lead to the ship running into troublesome waters and no one took any of the"
 print "appropriate measures, therefore it was destoryed and lost at sea."
 print "You somehow managed to get to an deserted island and pass out from exhaustion."
-print "There endless %s to your right, a dense %s up ahead, and steep rock %s to your left." % ( room1.getname(), room2.getname(), room3.getname() )
+print "There endless %s to your right, a dense %s up ahead, and steep rock %s to your left." % ( room1.getName(), room2.getName(), room3.getName() )
 
 while True:
 	print "Which way do you go?"
--- LearnPythonTheHardWay/ex45/room.py
+++ LearnPythonTheHardWay/ex45/room.py
@@ -1,7 +1,11 @@
 class room(object):
 
-	def __init__(self, name):
+	def __init__(self, name, scene):
 		self.name = name
+		self.scene = scene
 	
-	def getname(self):
+	def getName(self):
 		return self.name
+
+	def getScenario(self):
+		return self.scene
