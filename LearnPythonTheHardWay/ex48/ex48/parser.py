e632d25217ad2b387326cb6ebf10dc88e607da13
Ramon Gonzalez <rjgonza@gmail.com>
2012-12-03 23:53:45 -0500

    ex49

---
+++ LearnPythonTheHardWay/ex48/ex48/parser.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+def peek(word_list):
+	if word_list:
+		word = word_list[0]
+		return word[0]
+	else:
+		return None
+
+def match(word_list, expecting):
+	if word_list:
+		word = word_list.pop(0)
+
+		if word[0] == expecting:
+			return word
+		else:
+			return None
+	else:
+		return None
+
+def skip(word_list, word_type):
+	while peek(word_list) == word_type:
+		match(word_list, word_type)
