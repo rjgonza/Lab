7be6cd9db67ad08a88439f09fe4d6eafdbc5a60d
Ramon Gonzalez <rjgonza@gmail.com>
2012-12-06 20:45:43 -0500

    Added some more tests, and fixed the class

--- LearnPythonTheHardWay/ex48/ex48/parser.py
+++ LearnPythonTheHardWay/ex48/ex48/parser.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python
 
+class ParserError(Exception):
+	pass
+
+class Sentence(object):
+	def __init__(self, subject, verb, object):
+		# remember we take ('noun', 'princess') tuples and convert them
+		self.subject = subject[1]
+		self.verb = verb[1]
+		self.object = object[1]
+
 def peek(word_list):
 	if word_list:
 		word = word_list[0]
-		return word
+		return word[0]
 	else:
 		return None
 
 def match(word_list, expecting):
 	if word_list:
 		word = word_list.pop(0)
-
-		if word == expecting:
+		if word[0] == expecting:
 			return word
 		else:
 			return None
@@ -24,7 +33,6 @@ def skip(word_list, word_type):
 
 def parse_verb(word_list):
 	skip(word_list, 'stop')
-
 	if peek(word_list) == 'verb':
 		return match(word_list, 'verb')
 	else:
@@ -33,7 +41,6 @@ def parse_verb(word_list):
 def parse_object(word_list):
 	skip(word_list, 'stop')
 	next = peek(word_list)
-	
 	if next == 'noun':
 		return match(word_list, 'noun')
 	if next == 'direction':
@@ -44,14 +51,11 @@ def parse_object(word_list):
 def parse_subject(word_list, subj):
 	verb = parse_verb(word_list)
 	obj = parse_object(word_list)
-	
-	return Sentence(sujb, verb, obj)
+	return Sentence(subj, verb, obj)
 
 def parse_sentence(word_list):
 	skip(word_list, 'stop')
-	
 	start = peek(word_list)
-
 	if start == 'noun':
 		subj = match(word_list, 'noun')
 		return parse_subject(word_list, subj)
--- LearnPythonTheHardWay/ex48/tests/parser_tests.py
+++ LearnPythonTheHardWay/ex48/tests/parser_tests.py
@@ -4,13 +4,30 @@ from nose.tools import *
 from ex48 import parser
 
 def test_peek():
-	assert_equal(parser.peek(['first', 'second']), 'first')
+	assert_equal(parser.peek([('noun', 'princess'), ('verb', 'kill')]), 'noun')
 	assert_equal(parser.peek([]), None)
 
 def test_match():
-	assert_equal(parser.match(['first', 'second'], 'first'), 'first')
-	assert_equal(parser.match(['first', 'second'], 'third'), None)
-	assert_equal(parser.match([], 'third'), None)
+	assert_equal(parser.match([('noun', 'princess'), ('verb', 'kill')], 'noun'), ('noun', 'princess'))
+	assert_equal(parser.match([('noun', 'princess'), ('verb', 'kill')], 'verb'), None)
+	assert_equal(parser.match([], 'noun'), None)
 
 def test_skip():
-	assert_equal(parser.skip(['first', 'second'], 'first'), 'first')
+	test_list=[('noun', 'princess'), ('verb', 'kill')]
+	parser.skip(test_list, 'noun')
+	assert_equal(test_list[0], ('verb', 'kill'))
+
+def test_parse_verb():
+	test_list=[('stop', 'the'), ('verb', 'kill')]
+	assert_equal(parser.parse_verb(test_list), ('verb' ,'kill'))
+	assert_raises(parser.ParserError, parser.parse_verb, test_list)
+
+def test_parse_object():
+	test_list=[('stop', 'the'), ('noun', 'princess'), ('direction', 'north')]
+	assert_equal(parser.parse_object(test_list), ('noun', 'princess'))
+	assert_equal(parser.parse_object(test_list), ('direction', 'north'))
+	assert_raises(parser.ParserError, parser.parse_object, test_list)
+
+def test_parse_subject():
+	test_list=[('verb', 'kill'), ('stop', 'the'), ('noun', 'princess')]
+	assert_equal(parser.parse_subject(test_list, ('noun', 'princess')), object)
