bd97ced4260f2f1c78e203868bcb8f49e27ec5d5
Ramon Gonzalez <rjgonza@gmail.com>
2013-03-31 23:43:44 +0200

    Bound checking

--- Python/box_input.py
+++ Python/box_input.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
-
+import sys
 sentence = raw_input("Sentence: ")
 
 screen_width = 80
+if len(sentence) > screen_width:
+	print "Sorry the text you entered is too long to display"
+	#raise SystemExit
+	sys.exit()
 text_width = len(sentence)
 box_width = text_width + 6
 left_margin = (screen_width - box_width)//2 # // is interger division
