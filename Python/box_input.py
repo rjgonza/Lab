de6d92474ed2c332fb273f7fb1ae503284e5f937
Ramon Gonzalez <rjgonza@gmail.com>
2013-03-31 23:38:39 +0200

    Playing around some python stuff

---
+++ Python/box_input.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+sentence = raw_input("Sentence: ")
+
+screen_width = 80
+text_width = len(sentence)
+box_width = text_width + 6
+left_margin = (screen_width - box_width)//2 # // is interger division
+
+print
+print ' ' * left_margin + '+'  + '-' * (box_width-2) +   '+'
+print ' ' * left_margin + '|  '+ ' ' * text_width    + '  |'
+print ' ' * left_margin + '|  '+       sentence      + '  |'
+print ' ' * left_margin + '|  '+ ' ' * text_width    + '  |'
+print ' ' * left_margin + '+'  + '-' * (box_width-2) +   '+'
+print
