63dfa98dc9fe3d526754eab8dce9647fa733fd43
Ramon Gonzalez <rjgonza@gmail.com>
2013-04-01 00:05:30 +0200

    used a loop and dictonary to clean up

--- Python/print_prices.py
+++ Python/print_prices.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python
+import sys
+
 width = input("Please enter width: ")
-
 price_width = 10
-item_width = width - price_width
+inventory = { 'Apples' : 0.4 , 
+              'Pears' : 0.5 ,
+              'Cantaloupes' : 1.92,
+              'Dried Apricots (16 oz.)' : 8,
+              'Prunes (4 lbs.)' : 12
+            }
 
+for item in inventory:
+	if width < len(item) + price_width:
+		print "Please enter a larger size"
+		sys.exit()
+
+item_width = width - price_width
 header_format = '%-*s%*s'
 format = '%-*s%*.2f'
 
 print '=' * width
-
 print header_format % (item_width, 'Item', price_width, 'Price')
-
 print '-' * width
-
-print format % (item_width, 'Apples', price_width, 0.4)
-print format % (item_width, 'Pears', price_width, 0.5)
-print format % (item_width, 'Cantaloupes', price_width, 1.92)
-print format % (item_width, 'Dried Apricots (16 oz.)', price_width, 8)
-print format % (item_width, 'Prunes (4 lbs.)', price_width, 12)
-
+for item in inventory:
+	print format % (item_width, item, price_width, inventory[item])
 print '=' * width
