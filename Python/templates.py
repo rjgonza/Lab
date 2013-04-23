4549fcb4045601d363d2f022d1d7988662dc2a08
Ramon Gonzalez <rjgonza@gmail.com>
2013-04-22 21:46:32 -0400

    More messing around with the standart libraries

---
+++ Python/templates.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import fileinput, re
+
+# Matches fileds enclosed in square brackets:
+field_pat = re.compile(r'\[(.+?)\]')
+
+# We'll collect variables in this:
+scope = {}
+
+# This is used in re.sub:
+def replacement(match):
+	code = match.group(1)
+	try:
+		# If the field can be evaluated, return it:
+		return str(eval(code, scope))
+	except SyntaxError:
+		# Otherwise, execute the assignment in the same scope...
+		exec code in scope
+		# ...and return an empty string:
+		return ''
+
+# Get all the text as a single string:
+lines = []
+for line in fileinput.input():
+	lines.append(line)
+text = ''.join(lines)
+
+#Substitute all the occurrences of the field pattern:
+print field_pat.sub(replacement, text)
