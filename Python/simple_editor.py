1428c068a6f44d7dc7a714b9d5bf4ae101fe72fa
Ramon Gonzalez <rjgonza@gmail.com>
2013-05-03 17:42:59 -0400

    Adding a confirmation

--- Python/simple_editor.py
+++ Python/simple_editor.py
@@ -1,17 +1,37 @@
 #!/usr/bin/python
 import wx
+import sys
 def load(event):
+	if not filename.GetValue():
+		popup()
+		sys.exit(1)
 	file = open(filename.GetValue())
 	contents.SetValue(file.read())
 	file.close()
 
 def save(event):
 	file = open(filename.GetValue(), 'w')
+	confirm()
 	file.write(contents.GetValue())
 	file.close()
 
+def popup():
+	wx.MessageBox("There is no file specified you fool!\nShutting down...", "ERROR",
+		wx.OK | wx.ICON_ERROR)
+
+def confirm():
+	dlg = wx.SingleChoiceDialog(
+	self, "Confirmation", "Are you sure you want to overwrite the file " + file,
+	['Yes', 'No'],
+	wx.CHOICEDLG_STYLE
+	)
+	
+	if flg.ShowModal() == wx.ID_OK:
+		self.log.WriteText("You selected: %s\n" % dlg.GetStringSelection())
+	dlg.Destroy()	
+
 app = wx.App()
-win = wx.Frame(None, title="Simple Editor", size=(410, 335))
+win = wx.Frame(None, title="Config Editor", size=(410, 335))
 
 bkg = wx.Panel(win)
 
