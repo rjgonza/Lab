033151c18448b5d12d6b3b8da3f62a8bd2d1ed16
rjgonza <rjgonza@gmail.com>
2014-10-29 12:25:35 -0400

    Go screencast work

---
+++ Go/src/string/string_test.go
@@ -0,0 +1,19 @@
+package string
+
+import "testing"
+
+func Test(t *testing.T) {
+	var tests = []struct {
+		s, want string
+	}{
+		{"Backward", "drawkcaB"},
+		{"Hello, World", "dlroW ,olleH"},
+		{"", ""},
+	}
+	for _, c := range tests {
+		got := Reverse(c.s)
+		if got != c.want {
+			t.Errorf("Reverse(%q) == %q, want %q", c.s, got, c.want)
+		}
+	}
+}
