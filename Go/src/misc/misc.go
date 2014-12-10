ee3b6afaf80b0646826e50ac8dc624df5674974a
rjgonza <rjgonza@gmail.com>
2014-12-10 13:37:21 -0500

    Add æ to the mix

--- Go/src/misc/misc.go
+++ Go/src/misc/misc.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	ae := ""
-	for _, char := range []rune{1, 0xE6, 0346, 230, '\xE6', '\u00E6'} {
+	æ := ""
+	for _, char := range []rune{'æ', 0xE6, 0346, 230, '\xE6', '\u00E6'} {
 		fmt.Printf("[0x%X '%c'] ", char, char)
-		ae += string(char)
+		æ += string(char)
 		fmt.Print(char)
 	}
 
-	fmt.Print(ae)
+	fmt.Print(æ + "\n")
 }
