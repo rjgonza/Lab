3ecafb592e9574e0e8ccf17db4754580a76a383e
Ramon Gonzalez <rjgonza@gmail.com>
2015-03-22 09:57:53 -0400

    two more to go ;)

---
+++ Go/src/writingfiles/writingfiles.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	d1 := []byte("hello\ngo\n")
+	err := ioutil.WriteFile("dat1", d1, 0644)
+	check(err)
+
+	f, err := os.Create("dat2")
+	check(err)
+
+	//It is idiomatic to defer a close immediately after opening a file
+	defer f.Close()
+
+	d2 := []byte{115, 111, 109, 101, 10}
+	n2, err := f.Write(d2)
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n2)
+
+	n3, err := f.WriteString("writes\n")
+	fmt.Printf("wrote %d bytes\n", n3)
+
+	//Flushes writes to stable storage
+	f.Sync()
+
+	w := bufio.NewWriter(f)
+	n4, err := w.WriteString("buffered\n")
+	fmt.Printf("wrote %d bytes\n", n4)
+
+	//ensures that all buffered operations have been applied to the underlying writer
+	w.Flush()
+}
