2e22c77a09e46d1c176364db2d2fdb97d925280d
Ramon Gonzalez <rjgonza@gmail.com>
2020-02-18 21:43:50 -0500

    Just committing the changes to the hello-world runt tutorial. I like this little red crab

--- rust/hello-rust/src/main.rs
+++ rust/hello-rust/src/main.rs
@@ -1,5 +1,12 @@
-// use ferris_says::say;
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let out = b"Hello fellow Rustaceans!";
+    let width = 24;
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(out, width, &mut writer).unwrap();
 }
