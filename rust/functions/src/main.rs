0c8a870fef5aa7a6d553e243257dd8254f573333
Ramon Gonzalez <rjgonza@gmail.com>
2020-02-19 19:39:23 -0500

    Tiny bit more function work

--- rust/functions/src/main.rs
+++ rust/functions/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    another_function(5);
+    another_function(5, 6);
 }
 
-fn another_function(x: i32) {
+fn another_function(x: i32, y: i32) {
     println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
 }
