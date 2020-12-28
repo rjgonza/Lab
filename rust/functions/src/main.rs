f31aaabc61acb4d4a5a0d1ea17bb672908503e02
Ramon Gonzalez <rjgonza@gmail.com>
2020-12-27 20:15:37 -0500

    Messing around with rust again :)

--- rust/functions/src/main.rs
+++ rust/functions/src/main.rs
@@ -1,8 +1,15 @@
 fn main() {
     another_function(5, 6);
+    let returned_data = some_function(1.2, 1);
+    println!("returned_data: {}", returned_data);
 }
 
 fn another_function(x: i32, y: i32) {
     println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
+}
+
+fn some_function(_param_a: f32, _param_b: i128) -> f32 {
+    println!("I'm in some_function!");
+    1 as f32 // 1. or 1_f32 or 1f32
 }
--- rust/variables/src/main.rs
+++ rust/variables/src/main.rs
@@ -24,12 +24,15 @@ fn main() {
     let z = 'Z';
     let heart_eyes_cat = '😻';
 
+    println!("{} {} {} {} {} {}", x, y, t, f, c, z);
+
     println!("{}", heart_eyes_cat);
 
     let tup = (500, 6.4, 1);
 
     let (x, y, z) = tup;
 
+    println!("{} {} {}", x, y, z);
     println!("The value of y is: {}", y);
 
     let x: (i32, f64, u8) = (500, 6.4, 1);
@@ -39,4 +42,62 @@ fn main() {
     let six_point_four = x.1;
 
     let one = x.2;
+
+    println!("{} {} {}", five_hundred, six_point_four, one);
+
+    let example_str: &str = "Howdy";
+    let example_string: String = String::from("Partner");
+
+    let string_from_str: String = example_str.to_string();
+    let string_from_str2: String = "Some hardcoded string".to_string();
+
+    let string_from_hardcoded = String::from("Some hardcoded");
+    let string_from_str_var = String::from(example_str);
+
+    let str_from_string: &str = &example_str;
+
+    let combine_string_literals = ["first", "second"].concat();
+    let combine_with_format_macro = format!("{} {}", "first", "second");
+
+    let string_plus_str = example_string + example_str;
+
+    println!("{} {}", string_from_str, string_from_str2);
+    println!("{} {}", string_from_hardcoded, string_from_str_var);
+    println!("{}", str_from_string);
+    println!("{} {}", combine_string_literals, combine_with_format_macro);
+    println!("{}", string_plus_str);
+
+    let mut mut_string = String::new();
+    mut_string.push_str(example_str);
+    mut_string.push_str("Some hardcoded literal");
+    mut_string.push('m');
+
+    println!("{}", mut_string);
+
+    let a = String::from("a");
+    let b = String::from("b");
+    let combined = a + &b + &mut_string;
+
+    println!("{}", combined);
+
+    let str_from_substring: &str = &example_str[0..2];
+
+    println!("{}", str_from_substring);
+
+    let str_from_substring: &str = &example_str[0..=4];
+
+    println!("{}", str_from_substring);
+
+    let char_by_index = &example_str.chars().nth(1);
+
+    println!("{:?}", char_by_index);
+
+    match char_by_index {
+        Some(c) => println!("Found a char {}", c),
+        None => {}
+    }
+
+    if let Some(c) = example_str.chars().nth(2) {
+        println!("Found a char {}", c);
+    }
 }
