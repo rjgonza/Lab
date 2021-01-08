11da4e40a55b4e03ebfff7d50c84dd12426c7b29
Ramon Gonzalez <rjgonza@gmail.com>
2021-01-08 16:09:59 -0500

    More messing with rust

--- rust/functions/src/main.rs
+++ rust/functions/src/main.rs
@@ -2,6 +2,22 @@ fn main() {
     another_function(5, 6);
     let returned_data = some_function(1.2, 1);
     println!("returned_data: {}", returned_data);
+
+    some_procedure(2.3, 1);
+
+    some_str_procedure("test");
+
+    let string_slice_var: &str = "Howdy!";
+    some_str_procedure(string_slice_var);
+
+    let string_var = String::from("I'm a REAL String ;)");
+    some_str_procedure(&string_var);
+
+    some_string_procedures(string_var);
+}
+
+fn some_string_procedures(param: String) {
+    println!("I'm in some_string_procedure wih param {}", param);
 }
 
 fn another_function(x: i32, y: i32) {
@@ -9,7 +25,23 @@ fn another_function(x: i32, y: i32) {
     println!("The value of y is: {}", y);
 }
 
-fn some_function(_param_a: f32, _param_b: i128) -> f32 {
+fn some_str_procedure(param: &str) {
+    println!("I'm in some_str_procedure with param {}", param);
+}
+
+fn some_procedure(param_a: f32, param_b: i128) {
+    println!("I'm in some_procedure with a {} b {}", param_a, param_b);
+}
+
+fn some_function(param_a: f32, param_b: i128) -> f32 {
     println!("I'm in some_function!");
-    1 as f32 // 1. or 1_f32 or 1f32
+
+    if param_a < 100. {
+        let returned_var = 10.1 * param_a + param_b as f32;
+
+        returned_var
+    } else {
+        -1.
+    }
+    // 1 as f32 // 1. or 1_f32 or 1f32
 }
